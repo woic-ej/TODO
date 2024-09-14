48597969fd22ac68a4402a74cc89f9d4037ee979
woic-ej <woic0102@naver.com>
2024-09-14 19:54:16 +0900

    style: DetailTodoItem 퍼블리싱 완료

--- src/app/_components/check-list/DetailTodoItem.tsx
+++ src/app/_components/check-list/DetailTodoItem.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import React, { useState } from "react";
-import CheckBox from "@/app/_components/check-list/CheckBox";
+import CheckBox from "./CheckBox";
 
 interface Props {
   todo: string;
 }
 
-const CheckList: React.FC<Props> = ({ todo }) => {
+const DetailTodoItem: React.FC<Props> = ({ todo }) => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
 
   const handleClick = () => {
@@ -16,16 +16,16 @@ const CheckList: React.FC<Props> = ({ todo }) => {
 
   return (
     <div
-      className={`w-[527px] h-[50px] rounded-[27px] border-2 pl-3 border-slate-900 flex items-center nanum-regular-16 ${
+      className={`w-[996px] h-[50px] rounded-3xl border-2  border-slate-900 flex items-center justify-center  ${
         isChecked ? "bg-violet-100" : "bg-white"
       }`}
     >
       <div className="flex items-center gap-4">
         <CheckBox isChecked={isChecked} onClick={handleClick} />
-        <span className={`${isChecked && "line-through"}`}>{todo}</span>
+        <span className="underline nanum-bold-20 text-slate-900">{todo}</span>
       </div>
     </div>
   );
 };
 
-export default CheckList;
+export default DetailTodoItem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import CheckList from "./_components/check-list/CheckList";
+import DetailTodoItem from "./_components/check-list/DetailTodoItem";
+import TodoItem from "./_components/check-list/TodoItem";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-3">
-      <CheckList todo="강아지 목욕" />
+      <TodoItem todo="강아지 목욕" />
+      <DetailTodoItem todo="강아지 목욕" />
     </div>
   );
 }
