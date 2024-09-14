95456a1b04a5deff1b292c3126c842f5b2c25aaa
woic-ej <woic0102@naver.com>
2024-09-14 16:47:30 +0900

    setting: 폰트 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const nanumSquare = localFont({
+  src: "./fonts/NanumSquareR.ttf",
+  variable: "--font-nanum-square",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "TODO App",
+  description: "todo app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${nanumSquare.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-3">
+      <span className="text-[50px] text-amber-800">Home</span>
+      <span className="font-extrabold text-[50px]">Home</span>
+    </div>
+  );
+}
