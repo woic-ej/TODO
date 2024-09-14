9c59b88703171c0e16874be9a85b451ea56cdd3f
woic-ej <woic0102@naver.com>
2024-09-14 19:18:42 +0900

    MediumNavBar 퍼블리싱 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import LargeNavBar from "@/app/_components/navbars/LargeNavBar";
+import MediumNavBar from "./_components/navbars/MediumNavBar";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-3">
       <LargeNavBar />
+      <MediumNavBar />
     </div>
   );
 }
