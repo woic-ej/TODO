0f62e6ff4291abf10b830a01d8e450b34b20461e
woic-ej <woic0102@naver.com>
2024-09-16 15:25:33 +0900

    로고 클릭시 새로고침 기능 구현

--- src/app/_components/navbars/NavBar.tsx
+++ src/app/_components/navbars/NavBar.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import React from "react";
 import LogoImage from "@/assets/images/large-logo.png";
 import SmallLogo from "@/assets/images/small-logo.png";
 import Image from "next/image";
 
 const LargeNavBar = () => {
+  const handleClick = () => {
+    window.location.href = "/";
+  };
   return (
     <div className="w-full flex items-center pl-6 md:pl-[360px] h-[60px] border-b border-slate-200 bg-white">
-      <Image
-        src={LogoImage}
-        alt="로고 이미지"
-        className="w-[151px] h-[41px] hidden sm:block"
-      />
-      <Image
-        src={SmallLogo}
-        alt="작은 로고 이미지"
-        className="w-[71px] h-[40px] block sm:hidden"
-      />
+      <button onClick={handleClick}>
+        <Image
+          src={LogoImage}
+          alt="로고 이미지"
+          className="w-[151px] h-[41px] hidden sm:block"
+        />
+        <Image
+          src={SmallLogo}
+          alt="작은 로고 이미지"
+          className="w-[71px] h-[40px] block sm:hidden"
+        />
+      </button>
     </div>
   );
 };
